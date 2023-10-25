8bf1407ae8e205ae9dcc4142b70f2c809b414c0b
Deepak <b.deepakram@gmail.com>
2023-10-25 15:48:32 +0530

    initial integration of  front end and backend

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,10 @@ dotenv.config
 app.get("/",(req,res)=>{
     res.send("API is running");
 })
+app.get("/api/chat",(req,res)=>{
+    res.send(chats)
 
+})
 app.get("/api/chat/:id",(req,res)=>{
     //res.send(chats)
     const singleChat = chats.find(c =>c._id === req.params.id);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import { Route } from "react-router-dom"'
+import { Route } from "react-router-dom"
+import Homepage from './pages/Homepage';
+import Chatpage from './pages/Chatpage';
 function App() {
   return (
     <div className="App">
-    <Route path="/" component ={} />
-    
+    <Route path="/" component ={Homepage} exact/>
+    <Route path="/chats" component ={Chatpage} />
     </div>
   );
 }
